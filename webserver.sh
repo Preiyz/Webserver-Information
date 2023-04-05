5d74a36bfa26cb29fd998c7d8ff497bfba32b383
Ubuntu <ubuntu@ip-172-31-5-91.ec2.internal>
2023-04-05 02:55:49 +0000

    Webserver bash update

--- webserver.sh
+++ webserver.sh
@@ -10,9 +10,11 @@ then
 	echo "Welcome!"
 	sleep 1
 	echo
-	echo "Checking internet connection"
+	echo "Please give me a moment to check your internet connection"
 else
-	echo "User not authorized. Please contact administrator"
+	echo "Sorry, you are not authorized to run this script"
+	echo "Please contact your administrator for authorization"
+	echo "Have a good day"
 	exit
 fi
 
@@ -39,6 +41,7 @@ echo >> connectiontest
 apache=$(find /var/www/html -iname index.html)
 if [ "$apache" = "/var/www/html/index.html" ]
 then
+	echo "apache is already installed"
 	echo " I am now ready to collect your information"
 	sleep 2
 	sudo chmod 776 /var/www/html/index.html
@@ -46,7 +49,7 @@ then
 else
 	echo "Installing Apache2"
 	sleep 2
-	sudo apt-get install apache2
+	sudo apt-get install apache2 >> /dev/null
 	sleep 2
 	echo
 	echo "Apache2 is now installed"
@@ -55,36 +58,55 @@ fi
 #This sequence collects user information
 echo "Are you willing to provide your information? (Y/N)"
 read ans
-if [ $ans = Y ] || [ $ans = y ] || [ $ans = Yes ] || [ $ans = YES ] || [ $ans = yes ]
-then
-	echo "What is your first name?"
-	read name
-	echo
-	echo "What state do you reside?"
-	read state
-	echo
-	echo "How old are you?"
-	read age
-	echo
-	echo "How tall are you? (ft)"
-	read tall
-	echo
-	echo "What is your gender?"
-	read gender
-	echo
-	echo "Thank you for the provided information. Your information has been saved in the web database"
-	sleep 5
-	clear
-else
-	echo "Information will not be collected. Have a good day."
-	sleep 2
-	clear
-fi
+while [ $ans = Y ] || [ $ans = y ] || [ $ans = Yes ] || [ $ans = YES ] || [ $ans = yes ]
+do
+	echo "Thanks for willing to provide your information"
+	answ=N
+		while [ $answ = N ] || [ $answ = n ] || [ $answ = NO ] || [ $answ = no ] || [ $answ = No ]
+		do
+			echo "What is your first name?"
+			read name
+			echo
+			echo "What state do you reside?"
+			read state
+			echo
+			echo "How old are you?"
+			read age
+			echo
+			echo "How tall are you? (ft)"
+			read tall
+			echo
+			echo "What is your gender?"
+			read gender
+			echo
+			echo "Thank you for the provided information."
+			echo "
+			Name: $name
+			State: $state
+			Age: $age
+			Height: $tall ft
+			Gender: $gender"
+			echo
+			echo "Does this information look correct to you?"
+			read answ
+		done
 
 echo "
-Name: $name
-State: $state
-Age: $age
-Height: $tall ft
-Gender: $gender" >> /var/www/html/index.html
-echo >> /var/www/html/index.html
+<h2> Name: $name </h2>
+<h2> State: $state </h2>
+<h2> Age: $age </h2>
+<h2> Height: $tall ft </h2>
+<h2> Gender: $gender </h2>" >> /var/www/html/index.html
+echo "
+" >> /var/www/html/index.html
+
+echo
+echo
+echo "Processing...."
+sleep 2
+echo "I have saved your personal information to the web server"
+echo "Would you like to provide another persons information? (Y/N)"
+read ans
+done
+
+echo "Thanks for your time. See you later!"
